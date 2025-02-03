633bd4229eda3b76307ee4f5d727993e88903afc
hernirg <hernanrg85@gmail.com>
2025-02-03 16:06:56 +0100

    Initial Commit

---
+++ DevCollab/DevCollabApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevCollabApp.swift
+//  DevCollab
+//
+//  Created by Hernán Rodríguez on 3/2/25.
+//
+
+import SwiftUI
+
+@main
+struct DevCollabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
